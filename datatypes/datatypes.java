5849f24dd7963a21eb7148e17c11b4c7e1ff6a2f
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-01-29 13:25:52 +0530

    ss

---
+++ datatypes/datatypes.java
@@ -0,0 +1,27 @@
+public class datatypes {
+    public static void main(String[] args) {
+        /*there are some datatypes are available in java. they are int,float,char,double,string,boolean etc*/
+        int a = 6;               //to get integers we use int and we use myNum to store the values
+        System.out.println(a);
+
+        float myhieght = 7.5f;        //used to get small point values
+        System.out.println(myhieght);
+
+        char firstchar = 'a';            //used to get a single character
+        System.out.println(firstchar);
+
+        char firstChar= 97;             //also print a character by using ASCII values
+        System.out.println (firstChar);
+
+        double mysalary = 30000.58d;  //used to get big point values
+        System.out.println(mysalary);
+
+        String myName = "smily" ;     //used to get one word or sentence
+        System.out.println(myName);
+
+        boolean datasirpiisagoodworkplace = true;     //used to get true or false, yes or no etc
+        System.out.println(datasirpiisagoodworkplace );
+        boolean imateacher = false;
+        System.out.println(imateacher);
+    }
+    }
