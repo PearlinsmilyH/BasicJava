1e26e57eb798b42b145b0c81bbf0ab904cdb7e7e
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-01-30 10:39:03 +0530

    comments

---
+++ comments/comments.java
@@ -0,0 +1,6 @@
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("hai");           //this is used to display single line comments
+    }
+    /* this is used to display multiple line comments*/
+}
