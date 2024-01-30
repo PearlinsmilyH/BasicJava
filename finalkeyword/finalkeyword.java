0de041aacf91886855f559904f7d27b30ca62970
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-01-30 12:50:00 +0530

    finalkeyword

---
+++ finalkeyword/finalkeyword.java
@@ -0,0 +1,7 @@
+public class finalkeyword {
+    public static void main(String[] args)
+    {
+        final int a=5;                    //when we put final keyword...then the value will never be changed.It will be final
+        System.out.println("The value of a is : "+a);
+    }
+}
