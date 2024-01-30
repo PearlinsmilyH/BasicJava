07ec46feb988e24aad62213c32851b3d97f30330
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-01-30 15:12:11 +0530

    hashing

---
+++ hash/Linkedhash.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+    public class Linkedhash {
+        public static void main(String[] args) {
+            LinkedHashSet<String> veg = new LinkedHashSet<String>();
+            veg .add("carrot");
+            veg .add("beetroot");
+            veg .add("potato");
+            veg .add("onion");
+            veg .add("beans");
+            for (String i:veg)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+
---
+++ hash/Tree.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+    public class Tree {
+        public static void main(String[] args) {
+            TreeSet<String> fruites = new TreeSet<String>();
+            fruites.add("apple");
+            fruites.add("mango");
+            fruites.add("grapes");
+            fruites.add("guva");
+            fruites.add("pineapple");
+            for (String i:fruites)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+
