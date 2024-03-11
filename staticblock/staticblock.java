1279b36c6ffb5702a6b0441a1957424448dd0c3a
PearlinsmilyH <157511754+PearlinsmilyH@users.noreply.github.com>
2024-03-11 09:58:42 +0530

    Update staticblock.java

--- staticblock/staticblock.java
+++ staticblock/staticblock.java
@@ -1,15 +1,50 @@
-public class staticblock {
-    static int i;                //static variable
-    int j;
-static {
-    i = 50;                       // static block which called the static variable
-}
-}
-class sb{
-    public static void main(String[] args)
-{
-    System.out.println(staticblock.i);
-}
+public class Person {
+    // Private variables (encapsulated)
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of Person
+        Person person = new Person("John", 30);
 
+        // Accessing private variables using public methods
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+
+        // Modifying private variables using public methods
+        person.setName("Alice");
+        person.setAge(25);
+
+        // Accessing modified private variables
+        System.out.println("Updated Name: " + person.getName());
+        System.out.println("Updated Age: " + person.getAge());
+    }
+}
